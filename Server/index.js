fa19801648eda4234c7c5169222e59a4b84dea44
Ayotomiwadavid <ayotomiwadavid@proton.me>
2025-01-31 00:33:38 +0100

    saving progress

--- Server/index.js
+++ Server/index.js
@@ -2,3 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+//App Port
+const port = process.env.port || 8000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    console.log('Welcome home');
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+})
